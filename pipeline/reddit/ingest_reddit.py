92bf2f043dba26edc84cebc2001879dcec8ed335
atremante26 <atremante26@amherst.edu>
2025-07-16 13:03:15 -0400

    Updated ingest_reddit.py to upload to S3

--- pipeline/reddit/ingest_reddit.py
+++ pipeline/reddit/ingest_reddit.py
@@ -3,8 +3,20 @@ import json
 from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
 import os
+import boto3
 
 load_dotenv()
+
+# AWS S3 client
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+    region_name=os.getenv("AWS_DEFAULT_REGION")
+)
+
+S3_BUCKET = "mental-health-project-pipeline"
+
 
 # Load credentials
 reddit = praw.Reddit(
@@ -37,15 +49,12 @@ posts = [
     if datetime.fromtimestamp(p["created_utc"], tz=timezone.utc) > last_week
 ]
 
-# Timestamped output filenames
-today = datetime.now(timezone.utc).strftime("%Y-%m-%d")
-os.makedirs("data", exist_ok=True)
+# Get filenames
+today = datetime.today().strftime("%Y-%m-%d")
+raw_filename = f"reddit_raw_{today}.json"
+processed_filename = f"reddit_processed_{today}.json"
 
-# Save raw
-with open(f"data/raw/reddit_raw_{today}.json", "w") as f:
-    json.dump(posts, f, indent=2)
-
-# Save processed
+# Processed data
 processed = [
     {
         "subreddit": p["subreddit"],
@@ -58,5 +67,15 @@ processed = [
     for p in posts
 ]
 
-with open(f"data/processed/reddit_processed_{today}.json", "w") as f:
+# Save locally
+os.makedirs("data", exist_ok=True)
+
+with open(f"data/raw/{raw_filename}", "w") as f:
+    json.dump(posts, f, indent=2)
+
+with open(f"data/processed/{processed_filename}", "w") as f:
     json.dump(processed, f, indent=2)
+
+# Upload to S3
+s3.upload_file(f"data/{raw_filename}", S3_BUCKET, f"reddit-raw/{raw_filename}")
+s3.upload_file(f"data/{processed_filename}", S3_BUCKET, f"reddit-processed/{processed_filename}")
