e922a335c798935efac40ef5ec15eb73c6626b57
atremante26 <atremante26@amherst.edu>
2025-07-18 14:00:35 -0400

    load_snowflake.py updated

--- pipeline/snowflake/load_snowflake.py
+++ pipeline/snowflake/load_snowflake.py
@@ -1,38 +1,42 @@
 import os
 import snowflake.connector
 from datetime import datetime
+from dotenv import load_dotenv
 
+load_dotenv()
 
-def get_snowflake_connection():
+# Create the connection
+def snowflake_connection():
     return snowflake.connector.connect(
         user=os.getenv("SNOWFLAKE_USER"),
         password=os.getenv("SNOWFLAKE_PASSWORD"),
         account=os.getenv("SNOWFLAKE_ACCOUNT"),
         warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
         database=os.getenv("SNOWFLAKE_DATABASE"),
-        schema=os.getenv("SNOWFLAKE_SCHEMA"),
         role=os.getenv("SNOWFLAKE_ROLE"),
     )
 
-def run_sql(sql: str):
-    conn = get_snowflake_connection()
-    try:
-        with conn.cursor() as cur:
+# Run SQL with substitution
+def run_sql_from_file(filepath: str, date: str):
+    with open(filepath, "r") as f:
+        sql = f.read().replace("{{ ds_nodash }}", date)
+        with snowflake_connection().cursor() as cur:
             cur.execute(sql)
-    finally:
-        conn.close()
 
+# Loaders for each dataset
 def load_reddit_to_snowflake():
     today = datetime.today().strftime("%Y%m%d")
-    with open("sql/load_reddit.sql") as f:
-        run_sql(f.read().replace("{{ ds_nodash }}", today))
+    run_sql_from_file("pipeline/snowflake/reddit_sql/reddit_processed_load.sql", today)
 
 def load_cdc_to_snowflake():
     today = datetime.today().strftime("%Y%m%d")
-    with open("sql/load_cdc.sql") as f:
-        run_sql(f.read().replace("{{ ds_nodash }}", today))
+    run_sql_from_file("pipeline/snowflake/cdc_sql/cdc_processed_load.sql", today)
 
 def load_trends_to_snowflake():
     today = datetime.today().strftime("%Y%m%d")
-    with open("sql/load_trends.sql") as f:
-        run_sql(f.read().replace("{{ ds_nodash }}", today))
+    run_sql_from_file("pipeline/snowflake/trends_sql/trends_processed_load.sql", today)
+
+if __name__ == "__main__":
+    load_cdc_to_snowflake()
+    load_reddit_to_snowflake()
+    load_trends_to_snowflake()
