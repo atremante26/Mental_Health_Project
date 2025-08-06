57db702af39a6d346fdc73467e4597718dedf196
atremante26 <atremante26@amherst.edu>
2025-08-05 20:49:09 -0400

    Added static_ingestor.py to ingest static data sources

---
+++ pipeline/ingestion/static_ingestor.py
@@ -0,0 +1,9 @@
+from pipeline.ingestion import BaseIngestor
+import logging
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+class MentalHealthInTechSurveyIngestor(BaseIngestor):
+    
+
