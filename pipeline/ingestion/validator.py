9d88a91260e3714b099dc5059471fb1078b54245
atremante26 <atremante26@amherst.edu>
2025-09-16 12:54:02 -0400

    Added debug info for validator

--- pipeline/ingestion/validator.py
+++ pipeline/ingestion/validator.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import pandas as pd
 import great_expectations as gx
@@ -13,8 +14,17 @@ class Validator():
     def validate(self, df: pd.DataFrame, suite_name: str):
         """Perform data validation with Great Expectations"""
         try:
-            # Get context and load existing suite
-            context = gx.get_context(project_root_dir='/opt/airflow') # Use /opt/airflow as project root
+            logger.info(f"DAG execution environment:")
+            logger.info(f"User: {os.getuid()}")
+            logger.info(f"Working directory: {os.getcwd()}")
+            logger.info(f"Files readable: {os.access('/opt/airflow/gx/expectations/reddit_suite.json', os.R_OK)}")
+            
+            context = gx.get_context(project_root_dir='/opt/airflow')
+            logger.info(f"Context root directory: {context.root_directory}")
+            
+            available_suites = context.list_expectation_suite_names()
+            logger.info(f"Available suites: {available_suites}")
+
             suite = context.get_expectation_suite(expectation_suite_name=suite_name)
             
             # Create validator with existing suite
