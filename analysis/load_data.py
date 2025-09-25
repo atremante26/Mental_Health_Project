74caac205485fd11ad229da801e1bd459e179898
atremante26 <atremante26@amherst.edu>
2025-09-25 12:19:23 -0400

    Added debug for load_data for segment clustering model

--- analysis/load_data.py
+++ analysis/load_data.py
@@ -1,5 +1,11 @@
+import os
+import sys
+os.chdir('/Users/Andrew/Desktop/Computer Science/Mental_Health_Project')
+sys.path.append('.')
+
 import logging
 import pandas as pd
+from pathlib import Path
 from pipeline.snowflake.load_snowflake import snowflake_connection
 from config.data_paths import SQL_PATHS
 
@@ -7,13 +13,17 @@ from config.data_paths import SQL_PATHS
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Get root directory
+PROJECT_ROOT = Path(__file__).parent.parent
+
 def load_dataset(dataset_name):
     """Load a specific dataset by name using SQL files"""
     try:
         if dataset_name not in SQL_PATHS:
             raise ValueError(f"Unknown dataset: {dataset_name}")
         
-        sql_path = SQL_PATHS[dataset_name]
+        sql_path = PROJECT_ROOT / SQL_PATHS[dataset_name]
+        print("sql_path: ", sql_path)
         return load_from_sql_file(sql_path)
     
     except Exception as e:
@@ -38,3 +48,7 @@ def load_from_sql_file(sql_path):
     except Exception as e:
         logger.error(f"Error executing query from {sql_path}: {e}")
         return None
+    
+if __name__ == "__main__":
+    # Testing
+    data = load_dataset('tech_survey')
